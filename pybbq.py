11f1b01fc7c257812d82c78006a77632022495de
Austin Conn <austinc3030@gmail.com>
2020-05-19 15:15:05 -0400

    Restructured and methodize some stuff.

--- pybbq.py
+++ pybbq.py
@@ -3,8 +3,15 @@ from bluepy import btle
 import struct
 import logging
 import array
+import os
+import sys
+import signal
+
+
 
 address = "F8:30:02:48:75:17"
+
+
 
 SETTINGS_RESULT_UUID    = 0xfff1
 ACCOUNT_AND_VERIFY_UUID = 0xfff2
@@ -12,14 +19,20 @@ HISTORY_DATA_UUID       = 0xfff3
 REALTIME_DATA_UUID      = 0xfff4
 SETTINGS_DATA_UUID      = 0xfff5
 
+
+
 CREDENTIALS_MESSAGE          = bytes( [ 0x21, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0xb8, 0x22, 0x00, 0x00, 0x00, 0x00, 0x00 ] )
 REALTIME_DATA_ENABLE_MESSAGE = bytes( [ 0x0B, 0x01, 0x00, 0x00, 0x00, 0x00 ] )
 UNITS_F_MESSAGE              = bytes( [ 0x02, 0x01, 0x00, 0x00, 0x00, 0x00 ] )
 UNITS_C_MESSAGE              = bytes( [ 0x02, 0x00, 0x00, 0x00, 0x00, 0x00 ] )
 REQ_BATTERY_MESSAGE          = bytes( [ 0x08, 0x24, 0x00, 0x00, 0x00, 0x00 ] )
 
+
+
 class Delegate( btle.DefaultDelegate ):
     
+
+
     def handleNotification( self, cHandle, data ):
     
         if cHandle == 48:
@@ -29,6 +42,8 @@ class Delegate( btle.DefaultDelegate ):
             self.handleBattery( data )
 
     # End handleNotification
+
+
 
     def handleTemperature( self, data ):
         
@@ -42,6 +57,8 @@ class Delegate( btle.DefaultDelegate ):
 
     # End handleTemperature
     
+
+
     def handleBattery( self, data ):
         
         if data[ 0 ] != 36:
@@ -60,28 +77,51 @@ class Delegate( btle.DefaultDelegate ):
 
 # End Delegate( )
 
-# Connect
-client = btle.Peripheral( address )
-service = client.getServiceByUUID( "FFF0" )
-characteristics = service.getCharacteristics( )
-client.setDelegate( Delegate( ) ) 
-client.writeCharacteristic( characteristics[ 0 ].getHandle( ) + 1, b"\x01\x00", withResponse = True )
-client.writeCharacteristic( characteristics[ 3 ].getHandle( ) + 1, b"\x01\x00", withResponse = True )
 
-# Login
-characteristics[ 1 ].write( CREDENTIALS_MESSAGE, withResponse = True )
 
-# Enable Data
-characteristics[ 4 ].write( REALTIME_DATA_ENABLE_MESSAGE, withResponse = True )
+def toggleBluetoothInterface( ):
 
-# Set Farenheit
-characteristics[ 4 ].write( UNITS_C_MESSAGE, withResponse = True )
+    os.system( 'sudo hciconfig hci0 down' )
+    os.system( 'sudo hciconfig hci0 up' )
 
-# Request Battery
-characteristics[ 4 ].write( REQ_BATTERY_MESSAGE, withResponse = True )
+# End toggleBluetoothInterface( )
 
-# Request Temperature
-while True:
 
-    service.peripheral.readCharacteristic( characteristics[ 3 ].handle )
+
+def main( ):
+
+    toggleBluetoothInterface( )
+
+    # Connect
+    client = btle.Peripheral( address )
+    service = client.getServiceByUUID( "FFF0" )
+    characteristics = service.getCharacteristics( )
+    client.setDelegate( Delegate( ) ) 
+    client.writeCharacteristic( characteristics[ 0 ].getHandle( ) + 1, b"\x01\x00", withResponse = True )
+    client.writeCharacteristic( characteristics[ 3 ].getHandle( ) + 1, b"\x01\x00", withResponse = True )
+
+    # Login
+    characteristics[ 1 ].write( CREDENTIALS_MESSAGE, withResponse = True )
+
+    # Enable Data
+    characteristics[ 4 ].write( REALTIME_DATA_ENABLE_MESSAGE, withResponse = True )
+
+    # Set Farenheit
+    characteristics[ 4 ].write( UNITS_F_MESSAGE, withResponse = True )
+
+    # Request Battery
     characteristics[ 4 ].write( REQ_BATTERY_MESSAGE, withResponse = True )
+
+    # Request Temperature
+    while True:
+
+        service.peripheral.readCharacteristic( characteristics[ 3 ].handle )
+        characteristics[ 4 ].write( REQ_BATTERY_MESSAGE, withResponse = True )
+
+# End main( )
+
+
+
+# Run the main method
+if __name__ == '__main__':
+    main( )
